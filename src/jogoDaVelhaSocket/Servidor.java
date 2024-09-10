d73552dd42539743c4acb996832a16ff414c0560
Eliane Cordeiro <elianemcordeiro1@gmail.com>
2024-09-09 23:53:38 -0300

    refatoração case esperandoJogada

--- src/jogoDaVelhaSocket/Servidor.java
+++ src/jogoDaVelhaSocket/Servidor.java
@@ -1,4 +1,5 @@
 package jogoDaVelhaSocket;
+
 /**
  * 
  * implementar logica para aparecer a opção de jogada para os jogadas por vez 
@@ -22,7 +23,7 @@ import jogoDaVelhaSocket.utils.Transmissao;
 
 public class Servidor {
 
-	static int quantidadeDeJogadores = 0;	
+	static int quantidadeDeJogadores = 0;
 	static HashMap<Integer, Jogador> jogadoresMapeados = new HashMap<>();
 
 	public static void main(String args[]) throws Exception {
@@ -32,7 +33,6 @@ public class Servidor {
 		Recepcao recepcao = new Recepcao(socket);
 		Transmissao transmissao = new Transmissao(socket);
 		JogoDaVelha jogo = new JogoDaVelha();
-
 
 		Pacote pacote = null;
 
@@ -49,7 +49,7 @@ public class Servidor {
 					// Certifique-se de que o pacote não é null
 					if (pacote == null) {
 						// Pause o loop por um breve momento para não consumir CPU desnecessariamente
-						Thread.sleep(200);  // A pausa evita sobrecarregar o processador
+						Thread.sleep(200); // A pausa evita sobrecarregar o processador
 						break;
 					} else {
 						System.out.println("Recebeu o pacote: " + pacote.message().getFields()[0]);
@@ -62,80 +62,108 @@ public class Servidor {
 						povoarJogadoresMapeados(ip.getHostAddress(), porta);
 						quantidadeDeJogadores++;
 
-						
 						if (quantidadeDeJogadores == 1) {
-							transmissao.transmitirPacote(new Pacote(ip, porta, FabricaDeMensagem.criarMensagemDeEsperandoJogador()));
+							transmissao.transmitirPacote(
+									new Pacote(ip, porta, FabricaDeMensagem.criarMensagemDeEsperandoJogador()));
 						} else {
-							transmissao.transmitirPacote(new Pacote(ip, porta, FabricaDeMensagem.criarMensagemJogadoresProntos()));
+							transmissao.transmitirPacote(
+									new Pacote(ip, porta, FabricaDeMensagem.criarMensagemJogadoresProntos()));
 							jogo.status = StatusJogo.jogoIniciado;
 						}
 					}
-
 
 					break;
 
 				case jogoIniciado:
 					pacote = recepcao.receberMensagem();
 
-					if (quantidadeDeJogadores == 2 ) {
-						transmissao.transmitirPacote(new Pacote(pacote.address(),pacote.port(), FabricaDeMensagem.criarMensagemTabuleiro(jogo.imprimirTabuleiro())));
+					if (quantidadeDeJogadores == 2) {
+						//Imprimir tabuleiro para os jogadores
+						for(Jogador jogador : jogadoresMapeados.values()) {
+							transmissao.transmitirPacote(new Pacote(InetAddress.getByName(jogador.getIp()), jogador.getPorta(),
+									FabricaDeMensagem.criarMensagemTabuleiro(jogo.imprimirTabuleiro())));
+						}
+						
+						
 						for (Jogador jogador : jogadoresMapeados.values()) {
 
 							if (jogador.getId() == 0) {
-								//quando é sua vez 
-								transmissao.transmitirPacote(new Pacote(InetAddress.getByName(jogador.getIp()), jogador.getPorta(), FabricaDeMensagem.criarMensagemJogadorInicia()));
-								String tabuleiroVazio = jogo.imprimirTabuleiro();
+								// Mensagem para o jogador que inicia 
 								jogador.setSuaVez(true);
-
-
-							} else {
-								//vez do proximo jogador
-								transmissao.transmitirPacote(new Pacote(InetAddress.getByName(jogador.getIp()), jogador.getPorta(), FabricaDeMensagem.criarMensagemJogadorEspera()));
+								transmissao.transmitirPacote(new Pacote(InetAddress.getByName(jogador.getIp()),
+										jogador.getPorta(), FabricaDeMensagem.criarMensagemJogadorInicia()));
+								//String tabuleiroVazio = jogo.imprimirTabuleiro();
 								jogo.status = StatusJogo.esperandoJogada;
+							} else {
+								// vez do proximo jogador
+								transmissao.transmitirPacote(new Pacote(InetAddress.getByName(jogador.getIp()),
+										jogador.getPorta(), FabricaDeMensagem.criarMensagemJogadorEspera()));
+								//jogo.status = StatusJogo.esperandoJogada;
 							}
-						} 
-
+						}
 					}
 
-					if(jogo.quantidadeDejogadas < 9 && !jogo.venceu) {
-						
+					if (jogo.quantidadeDejogadas < 9 && !jogo.venceu) {
 
-
-					}else {
-						//jogo encerrado
+					} else {
+						// jogo encerrado
 					}
 					break;
+					
 				case esperandoJogada:
 					System.out.println("esperando jogada");
 					pacote = recepcao.receberMensagem();
-
-					for (Jogador jogador : jogadoresMapeados.values()) {
-						transmissao.transmitirPacote(new Pacote(InetAddress.getByName(jogador.getIp()), jogador.getPorta(), FabricaDeMensagem.criarMensagemSuaVez()));
-						                       		String tabuleiro = jogo.imprimirTabuleiro();
-						                        		System.out.println(tabuleiro);
-						                        		System.out.println("qualquer coisa aí 3");
-						                        	 transmissao.transmitirPacote(new Pacote(InetAddress.getByName(jogador.getIp()), jogador.getPorta(), FabricaDeMensagem.criarMensagemTabuleiro(tabuleiro)));
-					}
 					
 					Jogada jogada = null;
 					if (pacote != null) {
 
-						if (pacote.message().estaEnviandoJogada()&& pacote.message().getFields()[1] instanceof Jogada) {
+						if (pacote.message().estaEnviandoJogada()
+								&& pacote.message().getFields()[1] instanceof Jogada) {
 							jogada = (Jogada) pacote.message().getFields()[1];
-							
+
 							Pacote pacoteJogada = jogo.executarJogada(jogada, pacote, jogadoresMapeados);
 							transmissao.transmitirPacote(pacoteJogada);
-							jogo.status = StatusJogo.jogoIniciado;
-							
+							jogo.status = StatusJogo.esperandoJogada;
 						}
+					}
+					
+					for (Jogador jogador : jogadoresMapeados.values()) {
+						if(jogador.isSuaVez() == false) {
+							Mensagem mensagem = new Mensagem(new Object[] { TipoDeMensagem.aguardeSuaVez});
+							Pacote messageAguardeSuaVez = new Pacote(InetAddress.getByName(jogador.getIp()), jogador.getPorta(), mensagem);
+							transmissao.transmitirPacote(messageAguardeSuaVez);
+						}
+						
+						
+//						transmissao.transmitirPacote(new Pacote(InetAddress.getByName(jogador.getIp()),
+//								jogador.getPorta(), FabricaDeMensagem.criarMensagemSuaVez()));
+						
+						String tabuleiro = jogo.imprimirTabuleiro();
+//						                        		System.out.println(tabuleiro);
+//						                        		System.out.println("qualquer coisa aí 3");
+						
+						transmissao.transmitirPacote(new Pacote(InetAddress.getByName(jogador.getIp()),
+								jogador.getPorta(), FabricaDeMensagem.criarMensagemTabuleiro(tabuleiro)));
+					}
 
-					} 
-
+//					Jogada jogada = null;
+//					if (pacote != null) {
+//
+//						if (pacote.message().estaEnviandoJogada()
+//								&& pacote.message().getFields()[1] instanceof Jogada) {
+//							jogada = (Jogada) pacote.message().getFields()[1];
+//
+//							Pacote pacoteJogada = jogo.executarJogada(jogada, pacote, jogadoresMapeados);
+//							transmissao.transmitirPacote(pacoteJogada);
+//							jogo.status = StatusJogo.jogoIniciado;
+//
+//						}
+//
+//					}
 
 					break;
 				case enviandoTabuleiro:
-					
-					
+
 					break;
 				case jogoEncerrado:
 
